930eb6aa1de9d38e012f8de593a40fe1a026eabd
Inixio <inixio97@gmail.com>
2021-03-07 13:53:20 +0100

    First release

---
+++ index.js
@@ -0,0 +1,18 @@
+exports.generate = (length = 10) => {
+    const moves = ['F', 'B', 'R', 'L', 'U', 'D'];
+    const modifiers = ['2', '\'', ''];
+    let scramble = [];
+    if (isNaN(length) || length < 0 || length !== parseInt(length, 10)) {
+        throw new TypeError('The length must be an integer number greater than 0');
+    }
+    for (i = 0; i < length; i++) {
+        const move = moves[Math.floor(Math.random() * moves.length)];
+        const modifier = modifiers[Math.floor(Math.random() * modifiers.length)];
+        if (modifier === '2') {
+            scramble.push(`${modifier}${move}`);
+        } else {
+            scramble.push(`${move}${modifier}`);
+        }
+    }
+    return scramble;
+}
